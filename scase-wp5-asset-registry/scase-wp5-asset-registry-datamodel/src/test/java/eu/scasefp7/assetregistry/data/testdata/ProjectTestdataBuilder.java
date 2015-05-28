08424a4bc22ba912c21bef124147291693a6e6cf
Ivo Missler <ivo.missler@gmail.com>
2015-05-28 15:01:28 +0200

    added war project for deployment, fixed a lot of issues

--- scase-wp5-asset-registry/scase-wp5-asset-registry-datamodel/src/test/java/eu/scasefp7/assetregistry/data/testdata/ProjectTestdataBuilder.java
+++ scase-wp5-asset-registry/scase-wp5-asset-registry-datamodel/src/test/java/eu/scasefp7/assetregistry/data/testdata/ProjectTestdataBuilder.java
@@ -1,11 +1,14 @@
 package eu.scasefp7.assetregistry.data.testdata;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.akquinet.jbosscc.needle.db.testdata.AbstractTestdataBuilder;
 import eu.scasefp7.assetregistry.data.PrivacyLevel;
 import eu.scasefp7.assetregistry.data.Project;
 
 import javax.persistence.EntityManager;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  *
@@ -19,9 +22,16 @@ public class ProjectTestdataBuilder extends AbstractTestdataBuilder<Project> {
     @Override
     public Project build() {
         final Project project = new Project();
-        project.setName("Testproject");
+        project.setName("testproject");
+        project.setCreatedAt(new Date());
+        project.setCreatedBy("user");
         project.setPrivacyLevel(PrivacyLevel.PUBLIC);
         project.setArtefacts(Arrays.asList(new ArtefactTestdataBuilder(getEntityManager()).buildWithPayload()));
         return project;
+    }
+
+    public String toJSON(Project project) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(project);
     }
 }
